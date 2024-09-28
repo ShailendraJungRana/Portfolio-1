51ea535613b1653f3fd62e454b9ab2a1f0ef1301
Arun jR <cannon.arun1234@gmail.com>
2024-09-28 11:36:27 +0545

    Remaining project section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,9 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
+import CJ from '../../assets/theCJ.png';
 import freshBurger from '../../assets/fresh-burger.png';
 import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
+import Learning from '../../assets/Learning.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -10,30 +11,44 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
+      <div className='hover'>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
+          src={CJ}
+          h3="The Coding Journey"
+          p="Learning Web-App(Landing page)"
         />
+        <a href='https://e-learning-using-framer-motion.vercel.app/' target="_blank"><button >View</button></a>
+        <a href='https://github.com/ShailendraJungRana/E-learning-using--Framer-motion' target="_blank"><button >Code</button></a>
+
+        </div>
+        <div>
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          src={Learning}
+          
+          h3="Learning"
+          p="Learning-platform"
         />
+        <a href='https://ecommerce-web-snowy-beta.vercel.app/' target="_blank"><button >View</button></a>
+        <a href='https://github.com/ShailendraJungRana/Ecommerce-Web' target="_blank"><button >Code</button></a>
+        </div>
+        <div>
         <ProjectCard
           src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
           h3="Hipsster"
           p="Glasses Shop"
         />
+        <a href='https://ecommerce-web-snowy-beta.vercel.app/' target="_blank"><button >View</button></a>
+        <a href='https://github.com/ShailendraJungRana/Ecommerce-Web' target="_blank"><button >Code</button></a>
+        </div>
+        <div>
         <ProjectCard
           src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
           h3="FitLift"
           p="Fitness App"
         />
+         <a href='https://ecommerce-web-snowy-beta.vercel.app/' target="_blank"><button >View</button></a>
+        <a href='https://github.com/ShailendraJungRana/Ecommerce-Web' target="_blank"><button >Code</button></a>
+        </div>v
       </div>
     </section>
   );
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -21,17 +21,15 @@ function Skills() {
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
         <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="Framer-Motion" />
+        <SkillList src={checkMarkIcon} skill="Material UI" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="GitHub" />
+        <SkillList src={checkMarkIcon} skill="DaVinci Resolve" />
+        
       </div>
     </section>
   );
