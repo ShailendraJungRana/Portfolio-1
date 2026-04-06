2092c7c031295b616a793591f16c9ed459ca203f
Arun jR <cannon.arun1234@gmail.com>
2026-04-06 23:53:04 +0545

    Update on the project title

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,34 +1,45 @@
-import styles from './ProjectsStyles.module.css';
-import CJ from '../../assets/theCJ.png';
-import Hotel from '../../assets/hotel.jpg'
-import Mini from '../../assets/CICD.png';
-import scalable from '../../assets/scalable.png';
-import ProjectCard from '../../common/ProjectCard';
+import styles from "./ProjectsStyles.module.css";
+import CJ from "../../assets/theCJ.png";
+import Hotel from "../../assets/hotel.jpg";
+import Mini from "../../assets/CICD.png";
+import scalable from "../../assets/scalable.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
+
       <div className={styles.projectsContainer}>
-
-        <div>
-        <ProjectCard
-          src={scalable}
-          h3="Building a Scalable & Secure AWS Architecture (My DevOps Learning Journey)"
-
-        />
-        <a href='https://medium.com/@shailendera.jungrana/building-a-scalable-secure-aws-architecture-my-devops-learning-journey-8f06c02a84a2' target="_blank"><button >View</button></a>
-
+        
+        <div className={styles.cardWrapper}>
+          <ProjectCard
+            src={scalable}
+            h3="Building a Scalable & Secure AWS Architecture (My DevOps Learning Journey)"
+          />
+          <a
+            href="https://medium.com/@shailendera.jungrana/building-a-scalable-secure-aws-architecture-my-devops-learning-journey-8f06c02a84a2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>View</button>
+          </a>
         </div>
-    
-        <div>
-        <ProjectCard
-          src={Mini}
-          h3="CI/CD Pipeline with Code Quality Analysis and Artifact Management."
-          style={{ width: '200px', height: '150px' }} 
-        />
-        <a href='https://medium.com/@shailendera.jungrana/ci-cd-pipeline-with-code-quality-analysis-and-artifact-management-56bc53eefa29' target="_blank"><button >View</button></a>
+
+        <div className={styles.cardWrapper}>
+          <ProjectCard
+            src={Mini}
+            h3="CI/CD Pipeline with Code Quality Analysis and Artifact Management."
+          />
+          <a
+            href="https://medium.com/@shailendera.jungrana/ci-cd-pipeline-with-code-quality-analysis-and-artifact-management-56bc53eefa29"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>View</button>
+          </a>
         </div>
+
       </div>
     </section>
   );
